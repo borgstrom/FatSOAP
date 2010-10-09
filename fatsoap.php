c7b46d831e3d52274e9e692e2ff3ffe21772a46f
Evan Borgstrom <evan@fatbox.ca>
2010-10-09 11:07:43 -0400

    SOAP is now functional
    Create instances of SOAP_Client with a URL and array of namespaces.
    
    Invoke a RPC using call()
    
    XMLObject::with_namespace() has been updated to support an optional
    namespace, if not specified defaults to $this->namespace. This is to
    support the new own_namespace property, which if set will use a
    different name space for the base object than its properties.
    
    In serialize_writer we don't need to call startElement and endElement
    when we are dealing with other XMLObject's as they will call it during
    the call to XMLObject::create_xml()
    
    More documentation.

--- fatsoap.php
+++ fatsoap.php
@@ -1,15 +1,75 @@
 <?php
 /*
+ * FatSOAP
+ *
  * I hate SOAP, it should be COAP.
  *
  * These classes are the result of working with a very strict, broken
  * WSDL, web service.
+ *
+ * This is just a light shell, it does NOT support WSDL in any way and
+ * assumes that you'll create all of your objects with their correct
+ * target name spaces. In the future WSDL support may be added, but only
+ * if this gets more use.
+ *
+ * Requires: PHP >= 5.2.1, curl, XMLWriter
  */
 
 class SOAP_Client {
+	private $curl;
+
+	/*
+	 * this needs to be public so the XMLObject's can get it
+	 */
 	public $namespaces = array('soapenv' => 'http://schemas.xmlsoap.org/soap/envelope/');
-	public $xml;
 	public $writer;
+
+	/*
+	 * constructor sets url & optionally the namespaces
+	 */
+	public function __construct($url, $namespaces = null) {
+		if (($namespaces != null) and (is_array($namespaces))) {
+			$this->namespaces = $namespaces;
+		}
+		$this->curl = curl_init();
+		curl_setopt($this->curl, CURLOPT_URL, $url);
+		curl_setopt($this->curl, CURLOPT_POST, 1);
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+	}
+
+	/*
+	 * call invokes the SOAP function
+	 *
+	 * params:
+	 * $body - an XMLObject that will be used as the body
+	 * $header - if null no headers are added, else should be an object that extends XMLObject
+	 */
+	public function call($function, $body, $header = null) {
+		$this->create_xml($body, $header);
+		$data = $this->writer->outputMemory();
+
+		// we need to strip off the xml declaration
+		$lines = explode("\n", $data);
+		array_shift($lines);
+		$data = implode("\n", $lines);
+
+		$headers = array(
+			"SOAPAction: \"$function\"",
+			"Content-Type: text/xml",
+			"Content-Length: " . strlen($data),
+		);
+		curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+
+		$response = curl_exec($this->curl);
+
+		print $response;
+	}
+
+	public function show($function, $body, $header = null) {
+		$this->create_xml($body, $header);
+		return $this->writer->outputMemory();
+	}
 
 	/*
 	 * create_xml - builds the XML object for the request
@@ -65,7 +125,16 @@ class SOAP_Client {
  * consistent serialization with maximum flexibility
  */
 class XMLObject {
+	/*
+	 * namespace sets the namespace for the properties of this object
+	 */
 	public $namespace = null;
+
+	/*
+	 * own_namespace sets the namespace for the object itself, if its
+	 * null then it defaults to self->namespace
+	 */
+	public $own_namespace = null;
 
 	/*
 	 * soap_client & writer will hold references to the client & writer
@@ -89,21 +158,29 @@ class XMLObject {
 	/*
 	 * with_namespace is a utility function to prefix the namespace onto
 	 * a value, if it's not null
+	 *
+	 * params:
+	 * $name is the name that might be returned with the name space prefix
+	 * $namespace is optional and allows you to override $this->namespace
 	 */
-	private function with_namespace($name) {
-		if ($this->namespace !== null) {
+	private function with_namespace($name, $namespace = null) {
+		if ($namespace === null) {
+			$namespace = $this->namespace;
+		}
+
+		if ($namespace !== null) {
 			/*
 			 * allow name spaces to be specified by URL too
 			 */
-			$ns = $this->soap_client->namespace_by_url($this->namespace);
+			$ns = $this->soap_client->namespace_by_url($namespace);
 			if (isset($ns)) {
-				$this->namespace = $ns;
+				$namespace = $ns;
 			}
 
-			if (array_key_exists($this->namespace, $this->soap_client->namespaces)) {
-				$name = $this->namespace . ":" . $name;
+			if (array_key_exists($namespace, $this->soap_client->namespaces)) {
+				$name = $namespace . ":" . $name;
 			} else {
-				throw new Exception("You have specified an invalid name space (" . $this->namespace . ") for the " . get_class($this) . " class");
+				throw new Exception("You have specified an invalid name space ($namespace) for the " . get_class($this) . " class");
 			}
 		}
 		return $name;
@@ -133,9 +210,7 @@ class XMLObject {
 					$name = $cls;
 				}
 				$val->set_client(&$this->soap_client);
-				$this->writer->startElement($this->with_namespace($name));
-				$val->create_xml();
-				$this->writer->endElement();
+				$val->create_xml($this->namespace);
 			} else {
 				throw new Exception("serialize_writer got an object I can't serialize: $cls");
 			}
@@ -153,15 +228,18 @@ class XMLObject {
 	public function create_xml() {
 		$cls = get_class($this);
 
-		$this->writer->startElement($this->with_namespace($cls));
+		$this->writer->startElement($this->with_namespace($cls, $this->own_namespace));
 
 		$ref = new ReflectionClass($cls);
 		$props = $ref->getProperties();
 		foreach ($props as $prop) {
 			$name = $prop->getName();
-			if ($name == 'namespace') { 
-				continue;
+			switch ($name) {
+			case 'namespace':
+			case 'own_namespace':
+				continue 2; break; // jump to the next iteration in the foreach
 			}
+
 			if (isset($this->{$name})) {
 				$val = $this->{$name};
 				$this->serialize_writer($name, $val);
